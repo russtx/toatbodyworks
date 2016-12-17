aa6e0e3888b4374cd8456cbdb3f6bbc084079596
Russ berge <russ@toplinemediateam.com>
2016-12-17 14:06:54 -0500

    blog page added to the theme.

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="defaultSection ">
                     
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-image-414.jpg" alt="Logo" id="mobileOnly" class="homeImage" >
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-image-desktop.jpg" alt="Logo" id="desktopOnly" class="homeImage" >
 
 			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+                        <div class=" postContainer"><?php get_template_part('loop'); ?></div>
 
 			<?php get_template_part('pagination'); ?>
 
@@ -17,6 +17,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
--- loop-2.php
+++ loop-2.php
@@ -1,25 +1,20 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()): while (have_posts()) : the_post(); 
+
+
+
+?>
 
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class('defaultContent'); ?>>
 
 		<!-- post thumbnail -->
 		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-                
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail('medium'); ?>
+				<?php the_post_thumbnail('medium');  ?>
 			</a>
 		<?php endif; ?>
 		<!-- /post thumbnail -->
-                <div class="SocialLinks">
-                        <a href="https://api.instagram.com/oembed?url=http://instagr.am/p/fA9uwTtkSN/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" target="blank">
-                            <i class="fa fa-instagram" aria-hidden="true"></i>
-                        </a>
-                    <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" target="blank">
-                        <i class="fa fa-facebook-square" aria-hidden="true"></i>
-                    </a><br />
-                    
-                </div>
+
 		<!-- post title -->
 		<h2>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
